28303bd21dddb5929ac4a245aa325dc971e15768
Sate Pestage <noel.ar@protonmail.com>
2022-04-30 16:01:49 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ var mathOperators = [{sign: "+", method: (x = null, y = null) => { return x + y;
                      {sign: "-", method: (x = null, y = null) => { return x - y; }},
                      {sign: "*", method: (x = null, y = null) => { return x * y; }},
                      {sign: "÷", method: (x = null, y = null) => { return x / y; }}];
-var selectedOperator = Math.floor(Math.random() * mathOperators.length);
 var [num1,num2,result,attempts] = [[],[],[],0];
+var selectedOperator = Math.floor(Math.random() * mathOperators.length);
 
 function generateNumbers() {
   [num1,num2] = [Math.floor(Math.random() * 99), Math.floor(Math.random() * 99)];
@@ -13,8 +13,8 @@ function generateNumbers() {
     result = mathOperators[selectedOperator].method(num1,num2);
   }
 }
-function checkMath(callback=0,num1,num2,...numx) {
-  let opResult = num1 + num2 + numx.reduce((x,y)=>{return x+y});
+function checkMath(callback=0, num0,num9,...numx) {
+  let opResult = num0 + num9 + numx.reduce((x,y)=>{return x+y});
   (callback==undefined||callback==0) ? void(0) : callback(opResult);
 }
 function displaySomething(param=0) {
@@ -25,18 +25,18 @@ function displaySomething(param=0) {
  //   console.log(n);
  // })(0,0,0);
 function resetQuestion() {
-  {
-    generateNumbers();
-  }
+  generateNumbers();
   if (selectedOperator == 3) {
-    document.querySelectorAll("#question")[0].innerHTML = `${num1}&#160;${mathOperators[selectedOperator].sign}&#160;${Math.round(Math.sqrt(num2))}&#160;=`;
+    document.querySelectorAll("#question")[0].innerHTML =
+    `${num1}&#160;${mathOperators[selectedOperator].sign}&#160;${Math.round(Math.sqrt(num2))}&#160;=`;
   } else {
-    document.querySelectorAll("#question")[0].innerHTML = `${num1}&#160;${mathOperators[selectedOperator].sign}&#160;${num2}&#160;=`;
+    document.querySelectorAll("#question")[0].innerHTML =
+    `${num1}&#160;${mathOperators[selectedOperator].sign}&#160;${num2}&#160;=`;
   }
 }
 function clickHandler() {
+  attempts++;
   {
-    attempts++;
     (attempts >= 3) ? document.querySelectorAll(".btn_skip")[0].style.visibility = "visible" : void (0);
   }
   let [value,right_or_wrong] = [document.querySelector("#inputBox1").value, void (0)];
@@ -57,5 +57,6 @@ function clearBtn() {
   [this.style.visibility,attempts] = ["hidden",0];
   document.querySelectorAll("#inputBox1")[0].value = null;
   document.querySelectorAll(".result_style")[0].innerHTML = "";
+  selectedOperator = Math.floor(Math.random() * mathOperators.length);
   resetQuestion();
 }
